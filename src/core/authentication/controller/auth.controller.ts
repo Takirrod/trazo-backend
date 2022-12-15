5573cc2ad946179085c224393a2aebb9a2b6ee9f
LaBalaElmo <jrttaquichiri@gmail.com>
2022-12-15 15:19:44 -0400

    auth google

--- src/core/authentication/controller/auth.controller.ts
+++ src/core/authentication/controller/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Get, Post } from '@nestjs/common';
+import { Body, Get, Post, UseGuards } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
 import { Rol } from 'src/application/rol/entity/rol.entity';
 import { RolService } from 'src/application/rol/service/rol.service';
@@ -6,6 +6,7 @@ import { RegisterDto } from 'src/application/usuario/dto/register.dto';
 import { UserExists } from 'src/application/usuario/dto/user_exists.dto';
 import { Usuario } from 'src/application/usuario/entity/usuario.entity';
 import { UsuarioService } from 'src/application/usuario/service/usuario.service';
+import { GoogleOauthGuard } from '../guard/google-oauth.guard';
 import { AuthenticationService } from '../service/authentication.service';
 
 @Controller('auth')
@@ -16,7 +17,7 @@ export class AuthController {
     private rolService: RolService
   ){}
 
-  //@UseGuards(GoogleOauthGuard)
+  @UseGuards(GoogleOauthGuard)
   @Post('register')
   async registerUser(@Body() body: RegisterDto) {
     const user: Usuario = await this.usuarioService.saveUser(body)
@@ -28,7 +29,7 @@ export class AuthController {
     }
   }
 
-  //@UseGuards(GoogleOauthGuard)
+  @UseGuards(GoogleOauthGuard)
   @Post('login')
   async login(@Body() body){
     const {token, email} = body
--- src/core/authentication/guard/google-oauth.guard.ts
+++ src/core/authentication/guard/google-oauth.guard.ts
@@ -1,7 +1,8 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { OAuth2Client } from 'google-auth-library';
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
+// import { Observable } from 'rxjs';
+// import { OAuth2Client } from 'google-auth-library';
 import * as dotenv from 'dotenv'
+import { Request } from 'express';
 
 dotenv.config()
 
@@ -10,15 +11,30 @@ export class GoogleOauthGuard implements CanActivate {
   async canActivate(
     context: ExecutionContext,
   ) {
-    const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-    const body = context.switchToHttp().getRequest().body;
-    const ticket = await client.verifyIdToken({
-      idToken: body.token,
-      audience: process.env.GOOGLE_CLIENT_ID,
-    });
-    const payload = ticket.getPayload();    
+    // const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+    // const body = context.switchToHttp().getRequest().body;
+    // const ticket = await client.verifyIdToken({
+    //   idToken: body.token,
+    //   audience: process.env.GOOGLE_CLIENT_ID,
+    // });
+    // const payload = ticket.getPayload();    
     
-    return payload.aud === process.env.GOOGLE_CLIENT_ID && 
-    (payload.iss === 'accounts.google.com' || payload.iss === 'https://accounts.google.com');
+    // return payload.aud === process.env.GOOGLE_CLIENT_ID && 
+    // (payload.iss === 'accounts.google.com' || payload.iss === 'https://accounts.google.com');
+    const {
+      user,
+    } = context.switchToHttp().getRequest() as Request
+    
+    if (!user?.id) {
+      throw new BadRequestException(
+        `Es necesario que este autenticado para consumir este recurso.`
+      )
+    }
+    try {
+      if (user.id !== "GOOGLE") throw new ForbiddenException();
+    } catch (err) {
+      throw new ForbiddenException();
+    }
+    return true
   }
 }
