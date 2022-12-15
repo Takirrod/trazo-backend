b46fe8abb4926baf9483c90b4698117d53c4d7dc
LaBalaElmo <jrttaquichiri@gmail.com>
2022-12-15 15:39:01 -0400

    seeds

--- database/seeds/rol.sql
+++ database/seeds/rol.sql
@@ -1,3 +1,5 @@
 INSERT INTO "usuarios"."rol" ("rol", "descripcion", "es_publico") VALUES
 ('ADMINISTRADOR', 'Tiene acceso a todas las areas', 'false'),
-('USUARIO', 'Rol por defecto de todos los usuarios', 'true');
+('USUARIO', 'Rol por defecto de todos los usuarios', 'true'),
+('ESTUDIANTE', 'Estudiante de la Universidad Privada Boliviana(UPB)', 'true'),
+('DOCENTE', 'Docente de la Universidad Privada Boliviana(UPB)', 'true');
