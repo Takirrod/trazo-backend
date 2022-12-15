0d583f914b1043e979a4aefca8852236489a913c
LaBalaElmo <jrttaquichiri@gmail.com>
2022-12-15 08:45:18 -0400

    roles

--- src/application/rol/controller/rol.controller.ts
+++ src/application/rol/controller/rol.controller.ts
@@ -5,49 +5,47 @@ import { Rol } from '../entity/rol.entity';
 import { RolService } from '../service/rol.service';
 
 @Controller('rol')
-@UseGuards(AuthGuard('jwt'))
 export class RolController {
   constructor(
     private readonly rolService: RolService
   ) { }
 
-  @UseGuards(JwtAuthGuard)
   @Get('all')
   async getAllRoles() {
     return await this.rolService.getAllRoles()
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
   @Get('public')
   async getPublicRoles() {
     return await this.rolService.getAllPublicRoles()
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
   @Post('create')
   async createRol(@Body() body: Rol): Promise<Rol>{
     return this.rolService.saveRol(body)
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
   @Post('get')
   async getRoles(@Body() body: {roles: number[]}): Promise<Rol[]>{
     return await this.rolService.getRolesById(body.roles);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
   @Get('user/:id')
   async getRolesByUserId(@Param('id') usuarioId: number): Promise<Rol[]>{
     return await this.rolService.getRolesByUserId(usuarioId)
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
   @Put('update')
   async modifyRol(@Body() body: Partial<Rol>){
     return await this.rolService.updateRol(body)
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
   @Delete('delete/:id')
   async deleteRol(@Param('id') rolId: number){
     return await this.rolService.deleteRol(rolId)
